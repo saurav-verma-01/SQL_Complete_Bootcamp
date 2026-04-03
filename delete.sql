8420738f378bad3cee3f78f5cbd40249181d5822
SAURAV KUMAR VERMA <sauravkumarverma22@gmail.com>
2026-04-03 20:27:33 +0530

    Learning Data Manipulation in SQL

---
+++ delete.sql
@@ -0,0 +1,24 @@
+
+
+
+-- -- Update test 
+
+-- UPDATE customers
+-- SET score =  0 
+-- WHERE score IS NULL;
+
+-- SELECT * 
+-- FROM customers;
+
+-- -- SELECT * FROM customers WHERE id > 6;
+
+-- DELETE FROM customers 
+-- WHERE  id > 6;
+
+SELECT * 
+FROM persons;
+
+TRUNCATE TABLE persons;
+
+SELECT * 
+FROM persons;
